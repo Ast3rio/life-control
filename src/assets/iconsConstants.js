6c0144c468df86a73d8cae04d1aa2cc67008bb88
Slavnov Andrey <safilad@gmail.com>
2021-08-16 13:39:38 +0300

    start create loader

--- src/assets/iconsConstants.js
+++ src/assets/iconsConstants.js
@@ -1,5 +1,6 @@
 export {default as checkIco} from './icons/check-mark.svg';
 export {default as crossIco} from './icons/cross.svg';
 export {default as likeIco} from './icons/like.svg';
+export {default as loaderIco} from './icons/loader.svg';
 
 
