ca63ad2dd8cd49b07cdc8a67b8cd745c7e1ca755
Slavnov Andrey <safilad@gmail.com>
2021-11-12 21:44:52 +0200

    'add-styled-components'

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -5,31 +5,29 @@ import AppRouter from "./AppRouter";
 import {useDispatch, useSelector} from "react-redux";
 import {getCurrency} from "../../redux/action-creators/actionCreators";
 import {loaderIco} from "../../assets/iconsConstants";
+import {Container, ContentWrapper} from "../../styled/Common.styled";
 
 export const App = () => {
 
     const app = useSelector(state => state.app);
-
+    const {loading, error} = app;
     const dispatch = useDispatch();
 
     useEffect(() => {
         dispatch(getCurrency('USD', 'UAH'))
     }, [dispatch])
 
-    const {loading, error} = app;
+    if (loading) return <img src={loaderIco} alt="loader"/>
+    if (error) return error
 
     return (
         <div className={styles.app}>
             <Header/>
-            <div className="container">
-                <main className={styles.content}>
-                    {loading ? <div>
-                            <img src={loaderIco} alt="loader"/>
-                        </div> :
-                        error ? <div>{error}</div> :
-                            <AppRouter/>}
-                </main>
-            </div>
+            <Container>
+                <ContentWrapper>
+                    <AppRouter/>
+                </ContentWrapper>
+            </Container>
         </div>
     );
 };
---
+++ src/styled/Common.styled.js
@@ -0,0 +1,12 @@
+import styled from 'styled-components'
+
+export const Container = styled.div`
+   max-width: 1920px;
+   margin: 0 auto;
+   padding: 0 60px;
+`
+
+export const ContentWrapper = styled.main`
+   width: 100%;
+   min-height: 100vh;
+`
